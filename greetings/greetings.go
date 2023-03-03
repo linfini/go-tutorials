149ca83c3a0b32dcc6ed2feb38c0371ea4e93c1e
ZHAIJIE <zhaijie1@kingsoft.com>
2023-03-03 10:13:17 +0800

    :art: return a random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
@@ -11,6 +13,21 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty  name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// Go executes init functions automictically at program startup,after global variables have been initialized.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	//A sliceo of message formats
+	formats := []string{
+		"Hi,%v.Welcome!",
+		"Great to see you,%v",
+		"Hail,%v Well met!",
+	}
+	return formats[rand.Intn((len(formats)))]
 }
