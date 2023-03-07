562a59d725b49bb9b8a57c2b5c9def2967a24002
ZHAIJIE <zhaijie1@kingsoft.com>
2023-03-07 09:57:10 +0800

    :art: return greetings for multiple people

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -17,7 +17,7 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
-func Hleeos(names []string) (map[string]string, error) {
+func Hellos(names []string) (map[string]string, error) {
 	messages := make(map[string]string)
 	for _, name := range names {
 		message, err := Hello(name)
